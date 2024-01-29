5a86fab06e83867748a408dc7cfab83ae2ed001c
johnApplegate <applegation@gmail.com>
2024-01-29 13:27:26 +0000

    monday 29th jan

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.sidenav');
+//   console.dir(elems);
+//   var instances = M.Sidenav.init(elems);
+// });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    console.log('hello');
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-  });
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// console.dir(collapsibleElem);
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem);
+// console.dir(collapsibleInstance);
+
+// Or with jQuery
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+      
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('hello');
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
+});
 
 
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, {});
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, {});
+});
